65d2bd661ad4d3d6aca2c795d5546d74c85ee7d2
SoHenriquez <estebansotohenriquez@gmail.com>
2024-02-19 06:25:06 -0300

    Botón cv y detalles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,51 +1,38 @@
 import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { CarouselModule } from 'primeng/carousel';
-import { RippleModule } from 'primeng/ripple';
-import { TimelineModule } from 'primeng/timeline';
-import { TooltipModule } from 'primeng/tooltip';
+import { ScrollTopModule } from 'primeng/scrolltop';
 import { SkillsComponent } from '../skills/skills.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { TrayectoriaComponent } from '../trayectoria/trayectoria.component';
 import { ProyectsComponent } from '../proyects/proyects.component';
 import { HeroComponent } from '../hero/hero.component';
 import { DividerComponent } from '../divider/divider.component';
-
+import { TooltipModule } from 'primeng/tooltip';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-      TooltipModule, 
-      CardModule, 
-      RippleModule, 
-      NgFor, 
-      ButtonModule, 
-      TimelineModule, 
+      NgFor,  
       NgClass, 
-      CarouselModule,
       SkillsComponent,
       NavbarComponent,
       TrayectoriaComponent,
       ProyectsComponent,
       HeroComponent,
-      DividerComponent
+      DividerComponent,
+      ScrollTopModule,
+      TooltipModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   url = './data/data.json';
-  
   title = 'portafolio';
   
   
 
-  constructor() {
-    
-    
-  }
+  
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { TooltipModule } from 'primeng/tooltip';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, RippleModule, TooltipModule, HttpClientModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  CV_FILE_PATH = 'assets/Curriculum_vitae_Esteban_Soto_Henríquez.pdf'; 
+  constructor(private http: HttpClient) {
+    
+    
+  }
+  downloadFile() {
+    this.http.get(this.CV_FILE_PATH, { responseType: 'blob' })
+      .subscribe(response => {
+        const blob = new Blob([response], { type: 'application/pdf' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'Curriculum_vitae_Esteban_Soto_Henríquez.pdf'; // Set desired filename
+        link.click();
+        URL.revokeObjectURL(link.href); // Release memory after download
+      });
+  }
 }
